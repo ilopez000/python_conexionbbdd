92027f97c7d63412f1e5c18dc1342afa53e8dc07
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-03-28 16:54:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -47,6 +47,37 @@ def add_persona(conn, table_name, persona):
         print(e)
 
 
+#elimina de la tabla la persona con identificador=id
+def delete_persona(conn, table_name, id):
+    cursor = conn.cursor()
+    try:
+
+        query = f"DELETE FROM {table_name} WHERE id={id}"
+        cursor.execute(query)
+        conn.commit()
+        print(f'La persona com id {id} ha sido eliminada exitosamente en la tabla {table_name}')
+    except mysql.connector.Error as e:
+        print(e)
+
+
+#devuelve el resultado de ejecutar consulta_mysql en la tabla table_name
+#SELECT * FROM table_name WHERE id>100
+def resultado_consulta(conn,  consulta_mysql):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(consulta_mysql)
+        rows = cursor.fetchall()
+        res = []
+        for row in rows:
+            aux = Persona(row[0], row[1], row[2], row[3])
+            res.append(aux)
+
+        return res
+
+    except mysql.connector.Error as e:
+        print(e)
+
+
 if __name__ == '__main__':
     host = 'localhost'
     user = 'root'
@@ -55,22 +86,30 @@ if __name__ == '__main__':
 
     Personas=[]
 
-    conn = connect_to_db(host, user, password, database)
+    #conn = connect_to_db(host, user, password, database)
 
-    if conn:
-        Personas = fetch_personas(conn, 'datos')
-        close_connection(conn)
+    #if conn:
+        #Personas = fetch_personas(conn, 'datos')
+        #close_connection(conn)
 
 
-    conn2 = connect_to_db(host, user, password, database)
+    #conn2 = connect_to_db(host, user, password, database)
 
+    #if conn2:
+        #delete_persona(conn2, 'datos', 499)
+        #close_connection(conn2)
 
     #if conn2:
         #persona_nueva = Persona(501, 'Nombre501', 'Apellido1_501', 'Apellido2_501')
-        #add_persona(conn, 'datos', persona_nueva)
+        #add_persona(conn2, 'datos', persona_nueva)
         #close_connection(conn2)
 
 
+    conn3 = connect_to_db(host, user, password, database)
+    if conn3:
+        Personas=resultado_consulta(conn3, 'SELECT * FROM datos WHERE id<300')
+        close_connection(conn3)
+
 for p in Personas:
-    print(p)
+    p.imprimir_persona() #== print(p)
 
